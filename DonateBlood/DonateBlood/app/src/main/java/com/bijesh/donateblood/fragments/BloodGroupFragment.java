da050dad0701d80b395bc3a60ce92ff95309503f
unknown <bjayaprakash@che-bjayaprak.corp.checkfree.com>
2015-09-11 11:20:23 +0530

    added the images for default blood group dcreen

--- DonateBlood/DonateBlood/app/src/main/java/com/bijesh/donateblood/fragments/BloodGroupFragment.java
+++ DonateBlood/DonateBlood/app/src/main/java/com/bijesh/donateblood/fragments/BloodGroupFragment.java
@@ -8,12 +8,17 @@ import android.view.ViewGroup;
 
 import com.bijesh.donateblood.R;
 
+import at.markushi.ui.CircleButton;
+
 /**
  * Created by Bijesh on 06-09-2015.
  */
 public class BloodGroupFragment extends Fragment {
 
 
+    private CircleButton mCBAPositive,mCBANegative,mCBBPositive,mCBBNegative;
+    private CircleButton mCBOPositive,mCBONegative,mCBABPositive,mCBABNegative;
+    private CircleButton mCBA1Positive,mCBA1Negative,mCBA1BPositive,mCBA1BNegative;
 
     public BloodGroupFragment(){
 
@@ -28,6 +33,43 @@ public class BloodGroupFragment extends Fragment {
     }
 
     private void initComponents(View view){
+        mCBAPositive = (CircleButton) view.findViewById(R.id.circleButtonAPositive);
+        mCBAPositive.setImageResource(R.drawable.apositive);
+
+        mCBANegative = (CircleButton) view.findViewById(R.id.circleButtonANegative);
+        mCBANegative.setImageResource(R.drawable.anegative);
+
+        mCBBPositive = (CircleButton) view.findViewById(R.id.circleButtonBPositive);
+        mCBBPositive.setImageResource(R.drawable.bpositive);
+
+        mCBBNegative = (CircleButton) view.findViewById(R.id.circleButtonBNegative);
+        mCBBNegative.setImageResource(R.drawable.bnegative);
+
+        mCBOPositive = (CircleButton) view.findViewById(R.id.circleButtonOPositive);
+        mCBOPositive.setImageResource(R.drawable.opositive);
+
+        mCBONegative = (CircleButton) view.findViewById(R.id.circleButtonONegative);
+        mCBONegative.setImageResource(R.drawable.onegative);
+
+        mCBABPositive = (CircleButton) view.findViewById(R.id.circleButtonABPositive);
+        mCBABPositive.setImageResource(R.drawable.abpositive);
+
+        mCBABNegative = (CircleButton) view.findViewById(R.id.circleButtonABNegative);
+        mCBABNegative.setImageResource(R.drawable.abnegative);
+
+        mCBA1Positive = (CircleButton) view.findViewById(R.id.circleButtonA1Positive);
+        mCBA1Positive.setImageResource(R.drawable.a1positive);
+
+        mCBA1Negative = (CircleButton) view.findViewById(R.id.circleButtonA1Negative);
+        mCBA1Negative.setImageResource(R.drawable.a1negative);
+
+        mCBA1BPositive = (CircleButton) view.findViewById(R.id.circleButtonA1BPositive);
+        mCBA1BPositive.setImageResource(R.drawable.a1bpositive);
+
+        mCBA1BNegative = (CircleButton) view.findViewById(R.id.circleButtonA1BNegative);
+        mCBA1BNegative.setImageResource(R.drawable.a1bnegative);
+
+        
 
     }
 
